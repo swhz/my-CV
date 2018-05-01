b0f68d139575b7209a6dddbe2844cd914b166072
weizeng.shu <weizeng.shu@renren-inc.com>
2018-05-01 18:09:59 +0800

    更新about

--- src/components/about.js
+++ src/components/about.js
@@ -3,13 +3,13 @@ import Item from './item.js'
 
 const About = () => {
     const aboutArray = [
-        { class: 'iconfont icon-age', title: '年龄', desc: 24 },
+        { class: 'iconfont icon-age', title: '年龄', desc: 25 },
         { class: 'iconfont icon-sex', title: '性别', desc: '男' },
         { class: 'iconfont icon-education', title: '学历', desc: '本科'},
         { class: 'iconfont icon-University', title: '毕业院校', desc: '华中科技大学'},
         { class: 'iconfont icon-english', title: '英语水平', desc: 'CET-6'},
         { class: 'iconfont icon-coordinate', title: '籍贯', desc: '江西南昌'},
-        { class: 'iconfont icon-experience', title: '工作经验', desc: '2年'},
+        { class: 'iconfont icon-experience', title: '工作经验', desc: '3年'},
         { class: 'iconfont icon-intention', title: '求职意向', desc: '前端开发'},
         { class: 'iconfont icon-state', title: '状态', desc: '在职'}
         ]
